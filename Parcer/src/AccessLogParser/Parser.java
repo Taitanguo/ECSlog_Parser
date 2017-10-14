18d01621959c4b2822e96ad52d0a2c653c4e9398
Siyuan <siyuan.guo@emc.com>
2017-10-13 17:29:38 -0700

    parser with field name

--- Parcer/src/AccessLogParser/Parser.java
+++ Parcer/src/AccessLogParser/Parser.java
@@ -19,10 +19,11 @@ public class Parser{
 
     public final void processTheLastLine() throws IOException {
         try(Scanner scanner = new Scanner(fFilePath, ENCODING.name())) {
+            String result = "";
             while (scanner.hasNextLine()) {
-                scanner.nextLine();
+                result = scanner.nextLine();
             }
-            processLine(scanner.nextLine());
+            processLine(result);
         }
     }
 
@@ -31,7 +32,8 @@ public class Parser{
         for (String s : fields) {
             System.out.println(s);
         }
-        Scanner scanner = new Scanner(lastLine);
+        Scanner scanner;
+        scanner = new Scanner(lastLine);
         scanner.useDelimiter(" ");
         if(scanner.hasNext()) {
             String TIMESTAMP = scanner.next();
@@ -39,18 +41,30 @@ public class Parser{
             String LOCAL_IP = scanner.next();
             String REMOTE_IP = scanner.next();
             String USER_NAME = scanner.next();
+            String HTTP_METHOD = scanner.next();
+            String NAMESPACE = scanner.next();
+            String BUCKET = scanner.next();
+            String OBJECT_NAME = scanner.next();
+            String QUERY_STRING = scanner.next();
+            String PROTOCOL = scanner.next();
+            String STATUS_CODE = scanner.next();
+            String TOTAL_TIME = scanner.next();
+            String CONTENT_READ = scanner.next();
+            String CONTENT_COUNT = scanner.next();
+            String STORAGE_PROCESSING_TIME = scanner.next();
+
             log("TIMESTAMP : " + TIMESTAMP.trim() + ", REQUEST_ID : " + REQUEST_ID.trim());
         } else {
             log("Empty or invalid line. Unable to process.");
         }
     }
 
-    private static void log(Object aObject){
+    private static void log(Object aObject) {
         System.out.println(String.valueOf(aObject));
     }
 
-    public static void main(String... aArgs) throws IOException {
-        Parser parser = new Parser("C:\\Users\\guos6\\Desktop\\dataheadsvc-access");
+    public static void main(String[] args) throws IOException{
+        Parser parser = new Parser("C:\\Users\\guos6\\Desktop\\dataheadsvc-access.log");
         parser.processTheLastLine();
         log("Done.");
     }
