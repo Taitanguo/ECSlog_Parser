bf76585c63fff7d19d629ca8f5a6649147ff5398
Siyuan <siyuan.guo@emc.com>
2017-10-16 17:16:13 -0700

    parser with search function and latest updates log parsing function

--- Parcer/src/AccessLogParser/Parser.java
+++ Parcer/src/AccessLogParser/Parser.java
@@ -5,20 +5,25 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.io.*;
+
 
 /**
  * Created by guos6 on 10/13/2017.
  */
 public class Parser{
-    private final Path fFilePath;
-    private final static Charset ENCODING = StandardCharsets.UTF_8;
+    private final InputStream stream;
+
     //constructor
-    public Parser(String aFileName) {
-        fFilePath = Paths.get(aFileName);
+    public Parser(InputStream inputFileFromRemoteServer) {
+        stream = inputFileFromRemoteServer;
     }
 
-    public final void processTheLastLine() throws IOException {
-        try(Scanner scanner = new Scanner(fFilePath, ENCODING.name())) {
+    public synchronized void processTheLastLine() throws IOException {
+        try(Scanner scanner = new Scanner (stream)) {
             String result = "";
             while (scanner.hasNextLine()) {
                 result = scanner.nextLine();
@@ -27,13 +32,23 @@ public class Parser{
         }
     }
 
-    protected void processLine(String lastLine) {
-//        String[] fields = lastLine.split(" ");
-//        for (String s : fields) {
-//            System.out.println(s);
-//        }
+    public void searchBasedOnRequestId(String request_id) throws IOException {
+        try (Scanner scanner = new Scanner (stream)) {
+            String result;
+            while (scanner.hasNextLine()) {
+                result = scanner.nextLine();
+                if (result.indexOf(request_id) > -1) {
+                    processLine(result);
+                    break;
+                }
+            }
+            scanner.close();
+        }
+    }
+
+    protected synchronized void processLine(String curLine) {
         Scanner scanner;
-        scanner = new Scanner(lastLine);
+        scanner = new Scanner(curLine);
         scanner.useDelimiter(" ");
         if(scanner.hasNext()) {
             String TIMESTAMP = scanner.next();
@@ -57,6 +72,7 @@ public class Parser{
         } else {
             log("Empty or invalid line. Unable to process.");
         }
+        scanner.close();
     }
 
     private static void log(Object aObject) {
@@ -64,8 +80,48 @@ public class Parser{
     }
 
     public static void main(String[] args) throws IOException{
-        Parser parser = new Parser("C:\\Users\\guos6\\Desktop\\dataheadsvc-access.log");
-        parser.processTheLastLine();
-        log("Done.");
+        String user = "admin";
+        String password = "ChangeMe";
+        System.out.println("Please Input HostIp:");
+        Scanner scanner = new Scanner(System.in);
+        String host = scanner.next();
+        System.out.println("Please Input Rerquest_Id(no or Id):");
+        String request_id = scanner.next();
+        scanner.close();
+        int port=22;
+        String remoteFile="/opt/emc/caspian/fabric/agent/services/object/main/log/dataheadsvc-access.log";
+
+        try
+        {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(user, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            System.out.println("Establishing Connection...");
+            session.connect();
+            System.out.println("Connection established.");
+            System.out.println("Crating SFTP Channel.");
+            ChannelSftp sftpChannel = (ChannelSftp) session.openChannel("sftp");
+            sftpChannel.connect();
+            System.out.println("SFTP Channel created.");
+
+
+            InputStream dataheadsvc_access= null;
+            dataheadsvc_access = sftpChannel.get(remoteFile);
+            //search last line
+            Parser parser = new Parser(dataheadsvc_access);
+            parser.processTheLastLine();
+            //search function, notice: input stream cannot be reused
+            if (!request_id.equals("no")) {
+                dataheadsvc_access = sftpChannel.get(remoteFile);
+                Parser parserForSearch = new Parser(dataheadsvc_access);
+                parserForSearch.searchBasedOnRequestId(request_id);
+            }
+            log("Done");
+
+            session.disconnect();
+            sftpChannel.quit();
+        }
+        catch(Exception e){System.err.print(e);}
     }
 }
